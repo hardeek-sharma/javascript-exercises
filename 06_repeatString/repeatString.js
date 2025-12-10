f34ee7c052082b60710dfa0b8f44d527235b9915
hardeek-sharma <hardeeksa@gmail.com>
2025-12-09 22:07:15 -0500

    Add loop to continuously print string num amount of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -3,7 +3,13 @@ const repeatString = function(string, num) {
     return `ERROR`;
   }
 
-  
+  let final = ``;
+
+  for (let i = 0; i < num; i++) {
+    final += string;
+  }
+
+  return final;
 };
 
 // Do not edit below this line
