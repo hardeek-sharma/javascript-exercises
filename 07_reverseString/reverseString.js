db620a05ead45f1f117ef068fad7e07308e895b9
hardeek-sharma <hardeeksa@gmail.com>
2025-12-09 22:45:44 -0500

    Add loop to reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  strArray = string.split(``);
+  reversedArray = new Array(string.length);
 
+  let i = 0
+  for (letter of strArray) {
+    reversedArray[(string.length - 1) - i] = letter; 
+    i++;
+  }
+
+  return reversedArray.join(``)
 };
 
 // Do not edit below this line
