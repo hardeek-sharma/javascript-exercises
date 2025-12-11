58b9bf1f5b6b1cefad5e018b80b498c354e00345
hardeek-sharma <hardeeksa@gmail.com>
2025-12-10 22:15:40 -0500

    Solve problem

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let cel = (f - 32) * (5/9);
+  let roundedCel = Math.round(cel * 10) / 10;
+  
+  return roundedCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fah = c * (9/5) + 32;
+  let roundedFah = Math.round(fah * 10) / 10;
+
+  return roundedFah;
 };
 
 // Do not edit below this line
