ac8ffc4c852cd8f301134c22734092d4ff06dbb1
hardeek-sharma <hardeeksa@gmail.com>
2025-12-10 19:52:42 -0500

    Add negative checker and solved problem

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    let holder = min;
+    min = max;
+    max = holder;
+  }
 
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
