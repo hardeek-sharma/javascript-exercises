eea89546ed26cb4e6b9fac021b593771c102365d
hardeek-sharma <hardeeksa@gmail.com>
2025-12-10 21:18:43 -0500

    Solve problem

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  isDivisibleByFour = (year % 4 == 0);
+  isCentury = (year % 100 == 0);
+  isDivisibleByFourHundred = (year % 400 == 0);
 
+  if (isDivisibleByFour && (!isCentury || isDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
