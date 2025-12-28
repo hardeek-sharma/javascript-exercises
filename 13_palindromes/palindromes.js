09ebec11f934073ff44ec99589f9e652aa539e8f
hardeek-sharma <hardeeksa@gmail.com>
2025-12-27 23:48:25 -0500

    Solve problem

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function(str) {
+  let alphanumerical = '1234567890abcdefghijklmnopqrstuvwxyz';
 
+  let cleanStr = str
+    .toLowerCase()
+    .split('')
+    .filter(char => alphanumerical.includes(char))
+    .join('');
+
+  let holder = Array(str.length);
+  let arr = cleanStr.split('');
+
+  for (i = 0; i < str.length; i++) {
+    holder[i] = arr[str.length - i-1];
+  }
+
+  return holder.join('') == cleanStr;
 };
 
 // Do not edit below this line
