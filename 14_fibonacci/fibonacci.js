803b9d05daf712a52366fe63a18e91c743d7ca85
hardeek-sharma <hardeeksa@gmail.com>
2025-12-28 11:22:44 -0500

    Solve problem

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+  if (index < 0) return 'OOPS';
+  let nums = [0, 1];
+  for (i = 1; i < index; i++) {
+    nums.push(nums[i] + nums[i-1]);
+  }
 
+    return nums[index];
 };
 
 // Do not edit below this line
